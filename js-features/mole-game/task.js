8b32c9d1b00d583b41cea1dfc14749b6848324e2
Petr Brovkin <[bropet@mail.ru]>
2020-03-11 16:55:37 +0300

    Final

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,5 +1,24 @@
+let kills = document.getElementById("dead");
+let miss = document.getElementById("lost")
+
 function getHole(index) {
     for (let i = 1; i < 10; i++) {
-        let a = document.getElementById("")
+        let currentHole = document.getElementById(`${index}${i}`);
+        currentHole.onclick = function() {
+            if (currentHole.className.includes( 'hole_has-mole' ) === true) {
+                kills.textContent = parseFloat(kills.textContent) + 1;
+                if (kills.textContent == 10) {
+                    alert("Победа!"); 
+                    kills.textContent = 0, miss.textContent = 0; 
+                }
+            } else {
+                miss.textContent = parseFloat(miss.textContent) + 1;
+                if (miss.textContent == 5) {
+                    alert("Вы проиграли :(");
+                    kills.textContent = 0, miss.textContent = 0; 
+                }
+            }
+        }
     }
 }
+getHole("hole");
