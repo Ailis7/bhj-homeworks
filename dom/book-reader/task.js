f362e1d4577bab9b41b7e302a0952c15f5011006
Petr Brovkin <[bropet@mail.ru]>
2020-03-24 16:58:56 +0300

    dom corrections css

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -26,20 +26,21 @@ let fontChange = event => {
         parentControl.querySelector(".color_active").classList.remove("color_active");
         target.classList.add("color_active");
 
+        divBook.classList.remove("color_white");
         divBook.classList.remove("color_gray");
-        divBook.classList.remove("color_gray");
+        divBook.classList.remove("color_gray_text");
         divBook.classList.remove("color_black");
         divBook.classList.remove("color_whitesmoke");
 
         if (parentControl === text) {
             if (target.dataset.color !== undefined) divBook.classList.add(target.classList[1]);
             let bro = background.querySelector(".color_active");
-            if (target === bro) return;
+            //if (target === bro) return;
             if (bro.dataset.color !== undefined) divBook.classList.add(bro.classList[1]);
         } else {
             if (target.dataset.color !== undefined) divBook.classList.add(target.classList[1]);
             let bro = text.querySelector(".color_active");
-            if (target === bro) return;
+            //if (target === bro) return;
             if (bro.dataset.color !== undefined) divBook.classList.add(bro.classList[1]);
         }
         
